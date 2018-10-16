f8123f28c13c1d81d3f853076417a82b42351858
Tenji Tembo <tenji240@gmail.com>
2018-10-15 22:28:07 -0400

    #3 - Pagelayout update

--- src/layouts/Pagelayout.tsx
+++ src/layouts/Pagelayout.tsx
@@ -1,15 +1,34 @@
 import * as React from "react";
 import { Switch, Route } from "react-router-dom";
+import { Container, Menu } from "semantic-ui-react";
 import Dashboard from "../pages/dashboard";
 import Landing from "../pages/landing";
 
 const Pagelayout = () => {
   return(
     <div>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/dashboard" component={Dashboard} />
-      </Switch>
+      <Menu fixed="top">
+        <Container>
+          <Menu.Item as="a" header>
+            Klink.io
+          </Menu.Item>
+          <Menu.Menu position='right'>
+            <Menu.Item name='signup'>
+              Sign Up
+            </Menu.Item>
+
+            <Menu.Item name='help'>
+              Help
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+      <Container style={{ marginTop: '7em' }}>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Container>
     </div>
   );
 };
